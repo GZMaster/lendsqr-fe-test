f04df5439f662660d963de51112976359e010442
Osianic <56081021+Greg-Soap@users.noreply.github.com>
2023-01-03 17:18:21 +0100

    Login Complete

---
+++ Client/src/pages/Login.tsx
@@ -0,0 +1,27 @@
+import "../styles/main.scss";
+import logo from "../assets/images/logo.svg";
+import LoginImg from "../assets/images/pablo-sign-in 1.png";
+const Login = () => {
+  return (
+    <section className="Login">
+      <div className="left">
+        <img src={logo} alt="logo" className="logo" />
+        <img src={LoginImg} alt="Login Image" className="LoginImg" />
+      </div>
+      <div className="right">
+        <div className="title">Welcome!</div>
+        <p className="text">Enter details to login.</p>
+        <form>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+        </form>
+        <a href="#" className="forget">
+          FORGOT PASSWORD?
+        </a>
+        <div className="btn">LOG IN</div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
