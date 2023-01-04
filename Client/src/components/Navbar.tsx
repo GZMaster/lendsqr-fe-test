7dfb68796a0298249d3e3b01e3c0f7afd0c0f0c8
Osianic <56081021+Greg-Soap@users.noreply.github.com>
2023-01-04 08:18:21 +0100

    login responsiveness

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -1,7 +1,29 @@
-import React from "react";
-
+import logo from "../assets/images/logo.svg";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="Navbar">
+      <div className="left">
+        <div className="logo">
+          <img src={logo} alt="logo" className="logo" />
+        </div>
+        <div className="searchWrapper">
+          <input
+            type="text"
+            className="search"
+            placeholder="Search for anything"
+          />
+        </div>
+      </div>
+      <div className="docs">
+        <a href="#">Docs</a>
+      </div>
+      <div className="notification"></div>
+      <div className="profile">
+        <div className="profileImage"></div>
+        <div className="profileName">John Doe</div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- Client/src/pages/Login.tsx
+++ Client/src/pages/Login.tsx
@@ -4,21 +4,23 @@ import LoginImg from "../assets/images/pablo-sign-in 1.png";
 const Login = () => {
   return (
     <section className="Login">
-      <div className="left">
-        <img src={logo} alt="logo" className="logo" />
-        <img src={LoginImg} alt="Login Image" className="LoginImg" />
-      </div>
-      <div className="right">
-        <div className="title">Welcome!</div>
-        <p className="text">Enter details to login.</p>
-        <form>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-        </form>
-        <a href="#" className="forget">
-          FORGOT PASSWORD?
-        </a>
-        <div className="btn">LOG IN</div>
+      <div className="Container">
+        <div className="left">
+          <img src={logo} alt="logo" className="logo" />
+          <img src={LoginImg} alt="Login Image" className="LoginImg" />
+        </div>
+        <div className="right">
+          <div className="title">Welcome!</div>
+          <p className="text">Enter details to login.</p>
+          <form>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+          </form>
+          <a href="#" className="forget">
+            FORGOT PASSWORD?
+          </a>
+          <div className="btn">LOG IN</div>
+        </div>
       </div>
     </section>
   );
