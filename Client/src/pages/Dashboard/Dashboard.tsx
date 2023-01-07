7e4a09abb3d8d802535683c82daff0bd14a0234a
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-01-07 12:49:53 +0100

    solved pagination bug

--- Client/src/pages/Dashboard/Dashboard.tsx
+++ Client/src/pages/Dashboard/Dashboard.tsx
@@ -41,10 +41,14 @@ const Dashboard = () => {
   useEffect(() => {
     const getUsers = async () => {
       try {
-        const res = await axios.get(
-          "https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users"
-        );
-        setUsers(res.data);
+        await axios
+          .get(
+            "https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users"
+          )
+          .then((res) => {
+            setCurrentPage(1);
+            setUsers(res.data);
+          });
       } catch (error) {
         console.log(error);
       }
@@ -53,20 +57,12 @@ const Dashboard = () => {
   }, []);
   const [currentPage, setCurrentPage] = useState(2);
 
-  useEffect(() => {
-    setCurrentPage(1);
-  }, []);
-
   const userData = useMemo(() => {
     const firstPageIndex = (currentPage - 1) * PageSize;
     const lastPageIndex = firstPageIndex + PageSize;
     console.table(users);
     return users.slice(firstPageIndex, lastPageIndex);
   }, [currentPage]);
-  if (userData.length < 1) {
-  } else {
-    console.log("finally");
-  }
 
   return (
     <section className="Dashboard">
